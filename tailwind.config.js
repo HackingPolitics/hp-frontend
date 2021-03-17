81ded873ec62b15782a74fe2556fe155a4318bb3
Philipp Munzert <philipp@phmu.de>
2021-03-17 21:48:12 +0100

    fix purge css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,17 @@
 module.exports = {
-  purge: [],
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'helpers/*.js',
+      'plugins/**/*.js',
+      'nuxt.config.js',
+      'formulate.config.js',
+    ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
