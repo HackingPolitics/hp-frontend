efef99ba8c15bf4ddfb2fb6f0d20911ac674fe08
Philipp Munzert <philipp@phmu.de>
2021-05-17 21:46:34 +0200

    fix mobile nav

--- components/MainNav.vue
+++ components/MainNav.vue
@@ -43,7 +43,7 @@
             Icon when menu is closed.
           -->
             <svg
-              class="h-6 w-6"
+              class="h-6 w-6 text-white"
               :class="isMobileDropdownOpen ? 'hidden' : 'block'"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -62,7 +62,7 @@
             Icon when menu is open.
           -->
             <svg
-              class="h-6 w-6"
+              class="h-6 w-6 text-white"
               :class="isMobileDropdownOpen ? 'block' : 'hidden'"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -86,7 +86,7 @@
               <button
                 id="user-menu-button"
                 type="button"
-                class="bg-light-blue-500 rounded-full flex text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-light-blue-500 focus:ring-white"
+                class="bg-purple-500 rounded-full flex text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-purple-500 focus:ring-white"
                 aria-expanded="false"
                 aria-haspopup="true"
                 @click="toggleDropdown"
@@ -163,91 +163,96 @@
     </div>
 
     <!-- Mobile menu, show/hide based on menu state. -->
-    <div v-if="isMobileDropdownOpen" id="mobile-menu" class="lg:hidden">
-      <div class="pt-2 pb-3 px-2 space-y-1">
-        <a
-          href="#"
-          class="block rounded-md py-2 px-3 text-base font-medium text-white hover:text-white hover:bg-light-blue-400"
-          >Übersicht</a
-        >
+    <transition
+      enter-active-class="transition ease-out duration-100"
+      enter-class="transform opacity-0 scale-95"
+      enter-to-class="transform opacity-100 scale-100"
+      leave-active-class="transition ease-in duration-75"
+      leave-class="transform opacity-100 scale-100"
+      leave-to-class="transform opacity-0 scale-95"
+    >
+      <div v-if="isMobileDropdownOpen" id="mobile-menu" class="lg:hidden">
+        <div class="pt-2 pb-3 px-2 space-y-1">
+          <nuxt-link
+            to="/"
+            class="block rounded-md py-2 px-3 text-base font-medium text-white hover:text-white hover:bg-purple-400"
+          >
+            Übersicht
+          </nuxt-link>
 
-        <a
-          href="#"
-          class="block rounded-md py-2 px-3 text-base font-medium text-white hover:text-white hover:bg-light-blue-400"
-          >Jobs</a
-        >
+          <nuxt-link
+            to="/faq"
+            class="block rounded-md py-2 px-3 text-base font-medium text-white hover:text-white hover:bg-purple-400"
+          >
+            FAQ
+          </nuxt-link>
 
-        <a
-          href="#"
-          class="block rounded-md py-2 px-3 text-base font-medium text-white hover:text-white hover:bg-light-blue-400"
-          >Applicants</a
-        >
-
-        <a
-          href="#"
-          class="block rounded-md py-2 px-3 text-base font-medium text-white hover:text-white hover:bg-light-blue-400"
-          >FAQ</a
-        >
-      </div>
-      <div class="pt-4 pb-3 border-t border-light-blue-500">
-        <div class="flex items-center px-4">
-          <div class="flex-shrink-0">
-            <img
-              class="h-10 w-10 rounded-full"
-              src="https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixqx=XuwRpuUDYo&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-              alt=""
-            />
+          <nuxt-link
+            to="/collaboration"
+            class="block rounded-md py-2 px-3 text-base font-medium text-white hover:text-white hover:bg-purple-400"
+            >Collaboration</nuxt-link
+          >
+        </div>
+        <div class="pt-4 pb-3 border-t border-purple-500">
+          <div class="flex items-center px-4">
+            <div class="flex-shrink-0">
+              <img
+                class="h-10 w-10 rounded-full"
+                src="https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixqx=XuwRpuUDYo&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt=""
+              />
+            </div>
+            <div class="ml-3">
+              <div class="text-base font-medium text-white">Floyd Miles</div>
+              <div class="text-sm font-medium text-purple-200">
+                floydmiles@example.com
+              </div>
+            </div>
+            <button
+              class="ml-auto flex-shrink-0 bg-purple-500 rounded-full p-1 text-purple-200 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-purple-500 focus:ring-white"
+            >
+              <span class="sr-only">View notifications</span>
+              <!-- Heroicon name: outline/bell -->
+              <svg
+                class="h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                />
+              </svg>
+            </button>
           </div>
-          <div class="ml-3">
-            <div class="text-base font-medium text-white">Floyd Miles</div>
-            <div class="text-sm font-medium text-light-blue-200">
-              floydmiles@example.com
+          <div class="mt-3 px-2">
+            <a
+              href="#"
+              class="block rounded-md py-2 px-3 text-base font-medium text-purple-200 hover:text-white hover:bg-purple-400"
+              >Profil</a
+            >
+
+            <a
+              href="#"
+              class="block rounded-md py-2 px-3 text-base font-medium text-purple-200 hover:text-white hover:bg-purple-400"
+              >Einstellungen</a
+            >
+
+            <div
+              class="block rounded-md py-2 px-3 text-base font-medium text-purple-200 hover:text-white hover:bg-purple-400"
+              @click="$store.dispatch('auth/logout')"
+            >
+              Ausloggen
             </div>
           </div>
-          <button
-            class="ml-auto flex-shrink-0 bg-light-blue-500 rounded-full p-1 text-light-blue-200 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-light-blue-500 focus:ring-white"
-          >
-            <span class="sr-only">View notifications</span>
-            <!-- Heroicon name: outline/bell -->
-            <svg
-              class="h-6 w-6"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-              />
-            </svg>
-          </button>
-        </div>
-        <div class="mt-3 px-2">
-          <a
-            href="#"
-            class="block rounded-md py-2 px-3 text-base font-medium text-light-blue-200 hover:text-white hover:bg-light-blue-400"
-            >Profil</a
-          >
-
-          <a
-            href="#"
-            class="block rounded-md py-2 px-3 text-base font-medium text-light-blue-200 hover:text-white hover:bg-light-blue-400"
-            >Einstellungen</a
-          >
-
-          <div
-            class="block rounded-md py-2 px-3 text-base font-medium text-light-blue-200 hover:text-white hover:bg-light-blue-400"
-            @click="$store.dispatch('auth/logout')"
-          >
-            Ausloggen
-          </div>
         </div>
       </div>
-    </div>
+    </transition>
   </nav>
 </template>
 
@@ -281,7 +286,7 @@ export default defineComponent({
     const isMobileDropdownOpen = ref(false)
 
     const toggleMobileDropdown = (): void => {
-      isDropdownOpen.value = !isDropdownOpen.value
+      isMobileDropdownOpen.value = !isMobileDropdownOpen.value
     }
 
     const closeMobileDropdown = (): void => {
