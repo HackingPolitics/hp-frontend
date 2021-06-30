a6b57f9672bc65c4f0ae216f44428dff16f9fb1e
Philipp Munzert <philipp@phmu.de>
2021-06-30 15:40:00 +0200

    add base footer and base button

---
+++ components/BaseButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button
+    class="
+      flex
+      justify-center
+      py-2
+      px-4
+      text-sm
+      font-medium
+      rounded-md
+      bg-purple-500
+      text-white
+      hover:bg-purple-600
+      focus:outline-none focus:border-purple-700 focus:shadow-outline-purple
+      active:border-purple-700
+      transition
+      duration-150
+      ease-in-out
+    "
+    @click="$emit('click')"
+  >
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {
+    return {}
+  },
+})
+</script>
