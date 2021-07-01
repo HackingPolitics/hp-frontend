3df22f45e7f741eb59977f49f384c8842e622a51
Philipp Munzert <philipp@phmu.de>
2021-07-01 16:29:28 +0200

    refactor middleware for membership check

--- middleware/isProjectMember.ts
+++ middleware/isProjectMember.ts
@@ -1,32 +1,18 @@
 import { Middleware } from '@nuxt/types'
 
-const isLoggedIn: Middleware = async (context) => {
-  const guestRoutes = [
-    'login',
-    'registrieren',
-    'reset-password-token',
-    'passwort-vergessen',
-    'confirm-account-token',
-    'antraege',
-    'antraege-id',
-  ]
-  const { redirect, route, store, $axios } = context
+const isProjectMember: Middleware = (context) => {
+  const { redirect, route, store } = context
 
-  const user = store.state.auth.user
+  console.log(route.params.id)
 
-  const token = localStorage.getItem('auth_token')
+  const canEdit = store.getters['projects/canEditProject'](
+    parseInt(route.params.id)
+  )
 
-  if (route.name && !guestRoutes.includes(route.name)) {
-    if (!token) {
-      // If the user is not authenticated
-      console.log('redirect isLoggendin Middleware')
-      return redirect('/login')
-    }
-    if (token && !user) {
-      $axios.setToken(token, 'Bearer')
-      await store.dispatch('auth/refreshToken')
-    }
+  console.log(canEdit)
+  if (!canEdit) {
+    redirect(`/antraege/${route.params.id}`)
   }
 }
 
-export default isLoggedIn
+export default isProjectMember
