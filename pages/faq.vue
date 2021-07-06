2c593bf429e9e1cd52a5e2fa66cf0c9af45767c1
Philipp Munzert <philipp@phmu.de>
2021-07-06 15:43:21 +0200

    add htlm support

--- pages/faq.vue
+++ pages/faq.vue
@@ -1,7 +1,7 @@
 <template>
   <layouts-header-title title="FAQ">
     <div class="bg-white rounded-lg shadow overflow-hidden p-8 mt-16">
-      <div class="prose">{{ $t('faq.content') }}</div>
+      <div class="prose" v-html="$t('faq.content')"></div>
     </div>
   </layouts-header-title>
 </template>
