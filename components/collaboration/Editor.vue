bf95e888637b912bc99f2d54e677de051b323922
j-schumann <114239+j-schumann@users.noreply.github.com>
2021-08-05 18:44:42 +0200

    upd: collab for proposals upd: awareness for syncing areas & locked fields

--- components/collaboration/Editor.vue
+++ components/collaboration/Editor.vue
@@ -1,26 +1,133 @@
 <template>
-  <div v-if="editor" class="editor">
-    <collaboration-menu-bar class="editor__header" :editor="editor" />
-    <editor-content class="editor__content" :editor="editor" />
-    <div class="editor__footer">
-      <div :class="`editor__status editor__status--${status}`">
-        <template v-if="status === 'connected'">
-          {{ states.length }} user{{ states.length === 1 ? '' : 's' }} online
-        </template>
-        <template v-else> offline </template>
+  <div id="proposal-collab">
+    <h2 class="text-2xl font-normal mt-4 text-blue-gray-900">
+      Handlungsauftrag
+    </h2>
+    <div v-if="actionMandateEditor" class="editor">
+      <collaboration-menu-bar
+        class="editor__header"
+        :editor="actionMandateEditor"
+      />
+      <editor-content class="editor__content" :editor="actionMandateEditor" />
+      <div class="editor__footer">
+        <div :class="`editor__status editor__status--${status}`">
+          <template v-if="status === 'connected'">
+            {{ states.length }} user{{ states.length === 1 ? '' : 's' }} online
+          </template>
+          <template v-else> offline </template>
+        </div>
       </div>
-      <div>{{ savedAt }}</div>
+    </div>
+
+    <h2 class="text-2xl font-normal mt-4 text-blue-gray-900">
+      Bearbeitungskommentar
+    </h2>
+    <div v-if="commentEditor" class="editor">
+      <collaboration-menu-bar class="editor__header" :editor="commentEditor" />
+      <editor-content class="editor__content" :editor="commentEditor" />
+      <div class="editor__footer">
+        <div :class="`editor__status editor__status--${status}`">
+          <template v-if="status === 'connected'">
+            {{ states.length }} user{{ states.length === 1 ? '' : 's' }} online
+          </template>
+          <template v-else> offline </template>
+        </div>
+      </div>
+    </div>
+
+    <div id="collab-status">
+      <h2 class="text-2xl font-normal mt-4 text-blue-gray-900">
+        Collab-Status
+      </h2>
+      <div>Zeitstempel letzte Änderung Proposal via WS: {{ savedAt }}</div>
+
+      <h3 class="text-lg font-normal mt-4 text-blue-gray-900">
+        Online Clients (mehrere Browser(-tabs) etc.)
+      </h3>
       <ul>
         <li v-for="state in states" :key="state.clientId">
           <span
             :style="`background-color: ${state.user.color}; width: 1rem; height: 1rem; margin-right: 0.5rem; display: inline-block;`"
           />
           #{{ state.clientId }} {{ state.user.name }} (ID: {{ state.user.id }})
+          <div v-if="state.user.area">
+            Ist im Bereich "{{ state.user.area }}.
+          </div>
+          <div v-if="state.user.lockedField">
+            Bearbeitet Feld "{{ state.user.lockedField }}" seit
+            {{ state.user.lockedSince }}
+          </div>
         </li>
       </ul>
+
+      <h3 class="text-lg font-normal mt-4 text-blue-gray-900">
+        Bereiche der App
+      </h3>
+      <ul>
+        <li v-for="(area, name) in areas" :key="name">
+          {{ name }}
+          <ul>
+            <li v-for="(user, id) in area" :key="id">{{ user }} ({{ id }})</li>
+          </ul>
+        </li>
+      </ul>
+
+      <h3 class="text-lg font-normal mt-4 text-blue-gray-900">
+        Gesperrte Felder
+      </h3>
+      <ul>
+        <li v-for="(field, name) in lockedFields" :key="name">
+          {{ name }}: {{ field.locked ? 'gesperrt von' : 'frei' }}
+          <span v-if="field.locked">
+            {{ field.by }} (seit {{ field.since }})
+          </span>
+        </li>
+      </ul>
+
+      <h3 class="text-lg font-normal mt-4 text-blue-gray-900">
+        Online Users (nicht Clients!)
+      </h3>
+      <ul>
+        <li v-for="(user, id) in onlineUsers" :key="id">
+          {{ user }} ({{ id }})
+        </li>
+      </ul>
+
+      <h3 class="text-lg font-normal mt-4 text-blue-gray-900">Mein Status</h3>
+      Ich bin aktuell in Bereich ...
+      <select @change="changeArea">
+        <option></option>
+        <option>Thema</option>
+        <option>Handlung</option>
+        <option>Partner</option>
+      </select>
+      <br />
+
+      Ich bearbeite gerade ein Feld...
+      <button
+        class="
+          flex
+          justify-center
+          py-2
+          px-4
+          border
+          text-sm
+          font-medium
+          rounded-md
+          bg-purple-500
+          text-white
+          hover:border-purple-600 hover:text-600
+          focus:outline-none focus:border-purple-700 focus:shadow-outline-purple
+          active:border-purple-700
+          transition
+          duration-150
+          ease-in-out
+        "
+        @click="toggleFormLocked"
+      >
+        {{ currentUser.lockedField ? currentUser.lockedField : '[keins]' }}
+      </button>
     </div>
-    <button class="flex justify-center py-2 px-4 border text-sm font-medium rounded-md bg-purple-500 text-white hover:border-purple-600 hover:text-600 focus:outline-none focus:border-purple-700 focus:shadow-outline-purple active:border-purple-700 transition duration-150 ease-in-out" @click="toggleFormLocked">(Un-)Lock Form</button>
-    <span>{{ formLocked ? "Locked" : "Not locked" }}</span>
   </div>
 </template>
 
@@ -39,8 +146,67 @@ import {
 } from '~/services/hocuspocus/HocuspocusProvider'
 import { TokenUpdateMessage } from '~/services/hocuspocus/OutgoingMessages/TokenUpdateMessage'
 
+// @todo remove, testing only
 const getRandomElement = (list) => {
   return list[Math.floor(Math.random() * list.length)]
+}
+
+const reduceStates = (clients) => {
+  const areas = {}
+  const fields = {}
+  const users = {}
+
+  for (const client of clients) {
+    // ein Benutzer kann mit mehreren Clients online sein -> reduce
+    if (!users[client.user.id]) {
+      users[client.user.id] = client.user.name
+    }
+
+    // Ein Benutzer muss nicht zwingend in einem (relevanten) Bereich sein,
+    // ausserdem kann ein Benutzer mit mehreren Clients online sein und daher in
+    // mehreren Bereichen auftauchen
+    if (client.user.area) {
+      // Bereich wurde noch nie benutzt, erstmal anlegen...
+      if (!areas[client.user.area]) {
+        areas[client.user.area] = {}
+      }
+
+      // ein Benutzer kann mit mehreren Clients online & im gleichen Bereich sein -> reduce
+      if (!areas[client.user.area][client.user.id]) {
+        areas[client.user.area][client.user.id] = client.user.name
+      }
+    }
+
+    // Ein Benutzer kann mit mehreren Clients online sein, jeder Client kann aber immer nur ein
+    // Feld sperren -> der User kann nur mit dem Cursor in einem Feld sein etc., oder aber in keinem
+    if (client.user.lockedField) {
+      // Feld wurde noch nie benutzt, erstmal anlegen...
+      if (!fields[client.user.lockedField]) {
+        fields[client.user.lockedField] = {
+          locked: false,
+          since: null,
+          by: null,
+        }
+      }
+
+      if (
+        // es ist bisher nicht gesperrt
+        !fields[client.user.lockedField].locked ||
+        // oder der aktuelle client hat es später gelocked (um User rausschmeissen zu können
+        // die längere Zeit inaktiv sind, aktive Clients sollten ihren eigenen lockedSince-Stempel
+        // regelmässig aktualisieren, bspw mittels debounce wenn im Feld getippt wird)
+        fields[client.user.lockedField].since < client.user.lockedSince
+      ) {
+        fields[client.user.lockedField] = {
+          locked: true,
+          since: client.user.lockedSince,
+          by: client.user.name,
+        }
+      }
+    }
+  }
+
+  return { areas, fields, users }
 }
 
 // @todo Philipp: der Provider muss weiter oben in der Hierarchie angesiedelt werden damit er
@@ -54,10 +220,18 @@ export default defineComponent({
 
   data() {
     return {
+      // Der Status des aktuellen Users im aktuellen Client: Wo in der Anwendung ist er,
+      // welches Feld bearbeitet er gerade, welche Farbe wurde ihm zugewiesen (Cursor im WYSIWYG).
+      // Sollte von der App mittels setAwarenessState() aktualisiert werden, das sendet das Update
+      // gleichzeitig an alle User.
+      // Wird
       currentUser: {
         name: this.$auth.user.username || '[not logged in]',
         id: this.$auth.user.id | 0,
         color: this.getRandomColor(),
+        lockedField: null,
+        lockedSince: null,
+        area: null,
       },
 
       // demo property to sync form is locked/yes no
@@ -65,7 +239,10 @@ export default defineComponent({
 
       provider: null,
       yDoc: null,
-      editor: null,
+
+      // @todo add introduction, reasoning
+      actionMandateEditor: null,
+      commentEditor: null,
 
       // holds the timestamp when the WS server last sent the data (successfully) to the backend
       savedAt: -1,
@@ -73,8 +250,13 @@ export default defineComponent({
       // connecting | connected | disconnected
       status: 'connecting',
 
-      // holds the list of online users
+      // holds the list of unmodified states
       states: [],
+
+      // holds the decomposed awarenessStates
+      areas: {},
+      lockedFields: {},
+      onlineUsers: {},
 
       // internal
       syncState: null,
@@ -87,23 +269,22 @@ export default defineComponent({
     this.syncState = this.ydoc.getMap('syncState')
 
     this.ydoc.on('update', () => {
-      this.formLocked = this.syncState.get('formLocked')
       this.savedAt = this.syncState.get('savedAt')
     })
 
     this.provider = new HocuspocusProvider({
       url: this.$config.WS_URL,
-      name: 'project-1', // @todo replace "1" with the real project ID
+      name: 'proposal-1', // @todo replace "1" with the real project ID
       document: this.ydoc,
       parameters: { authToken: this.getToken() },
       onConnect: () => {
         this.status = 'connected'
       },
-      onMessage: ({ event, message }) => {
+      onMessage: ({ _event, _message }) => {
         // any document update is applied *after* this hook
         // console.log(`[message] ◀️ ${message.name}`, event)
       },
-      onOutgoingMessage: ({ message }) => {
+      onOutgoingMessage: ({ _message }) => {
         // console.info(`[message] ▶️ ${message.name} (${message.description})`)
       },
       onClose: ({ event }) => {
@@ -111,6 +292,7 @@ export default defineComponent({
         if (event.code === 4403) {
           // prevent the provider from endlessly retrying
           this.provider.disconnect()
+          // eslint-disable-next-line no-console
           console.log('WS disconnected, authentication failure')
         }
 
@@ -118,6 +300,7 @@ export default defineComponent({
       },
       onDisconnect: () => {
         if (this.provider.shouldConnect) {
+          // eslint-disable-next-line no-console
           console.log(
             'WS connection closed unexpectedly, trying to reconnect...'
           )
@@ -125,10 +308,15 @@ export default defineComponent({
       },
       onAwarenessChange: ({ states }) => {
         this.states = states
+        const reduced = reduceStates(states)
+        this.areas = reduced.areas
+        this.lockedFields = reduced.fields
+        this.onlineUsers = reduced.users
+        console.log(this.lockedFields)
       },
     })
 
-    this.editor = new Editor({
+    this.actionMandateEditor = new Editor({
       extensions: [
         StarterKit.configure({
           history: false,
@@ -139,7 +327,27 @@ export default defineComponent({
         }),
         Collaboration.configure({
           document: this.ydoc,
-          field: 'description',
+          field: 'actionMandate',
+        }),
+        CollaborationCursor.configure({
+          provider: this.provider,
+          user: this.currentUser,
+        }),
+      ],
+    })
+
+    this.commentEditor = new Editor({
+      extensions: [
+        StarterKit.configure({
+          history: false,
+        }),
+        Highlight,
+        CharacterCount.configure({
+          limit: 10000,
+        }),
+        Collaboration.configure({
+          document: this.ydoc,
+          field: 'comment',
         }),
         CollaborationCursor.configure({
           provider: this.provider,
@@ -153,7 +361,7 @@ export default defineComponent({
     // * $auth.$storage.watchState() did not trigger in tests (with: token, _token, _token.local, auth._token.local as key)
     this.timer = setTimeout(this.sendTokenUpdate.bind(this), 5000)
 
-    //this.setAwarenessState()
+    // this.setAwarenessState()
   },
 
   beforeDestroy() {
@@ -215,17 +423,30 @@ export default defineComponent({
     },
 
     toggleFormLocked() {
-      this.syncState.set('formLocked', !this.formLocked)
+      if (this.currentUser.lockedField) {
+        this.setAwarenessState({
+          lockedField: null,
+          lockedSince: null,
+        })
+      } else {
+        this.setAwarenessState({
+          lockedField: this.getRandomField(),
+          lockedSince: Date.now(),
+        })
+      }
     },
 
-    // @todu unused
+    changeArea(e) {
+      this.setAwarenessState({ area: e.currentTarget.value })
+    },
+
     setAwarenessState(values = {}) {
-      this.me = {
-        ...this.me,
+      this.currentUser = {
+        ...this.currentUser,
         ...values,
       }
 
-      this.provider.setAwarenessField('user', this.me)
+      this.provider.setAwarenessField('user', this.currentUser)
     },
 
     getRandomColor() {
@@ -237,6 +458,16 @@ export default defineComponent({
         '#70CFF8',
         '#94FADB',
         '#B9F18D',
+      ])
+    },
+
+    getRandomField() {
+      return getRandomElement([
+        'Problem',
+        'Titel',
+        'Impact',
+        'Handlungsauftrag 2',
+        'Partner 1',
       ])
     },
   },
