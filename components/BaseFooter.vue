bafae6f2ae731b00bfbd903dc800ae88687329d3
Philipp Munzert <philipp@phmu.de>
2021-09-02 10:26:25 +0200

    #254 link supporter logos

--- components/BaseFooter.vue
+++ components/BaseFooter.vue
@@ -40,18 +40,7 @@
       </div>
       <div>
         <div class="mt-8 md:mt-0 md:order-1 flex items-end space-x-4">
-          <div class="flex items-center">
-            <img
-              src="https://i0.wp.com/blog.okfn.org/files/2017/12/22137279_1679687182104997_6759961652435307500_o.jpg"
-              alt="Gefördert durch BMBF"
-              class="h-14"
-            />
-            <img
-              src="https://zukunftsstadt.de/wp-content/uploads/2019/12/BMBF_gefo%CC%88rdert-vom_deutsch.jpg"
-              alt="Gefördert durch BMBF"
-              class="h-28"
-            />
-          </div>
+          <supporter-logos></supporter-logos>
         </div>
       </div>
     </div>
---
+++ components/SupporterLogos.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex items-center">
+    <a href="https://prototypefund.de/" alt="Prototype Fund" target="blank">
+      <img
+        src="https://i0.wp.com/blog.okfn.org/files/2017/12/22137279_1679687182104997_6759961652435307500_o.jpg"
+        alt="Gefördert durch BMBF"
+        class="h-14"
+      />
+    </a>
+    <a
+      href="https://www.bmbf.de/"
+      alt="Bundesministerium für Bildung und Forschung"
+      target="blank"
+    >
+      <img
+        src="https://zukunftsstadt.de/wp-content/uploads/2019/12/BMBF_gefo%CC%88rdert-vom_deutsch.jpg"
+        alt="Gefördert durch BMBF"
+        class="h-28"
+    /></a>
+  </div>
+</template>
